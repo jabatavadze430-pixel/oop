97247cf106735b3ac0160cf95ef1bd84ad0eb9a4
jabatavadze430-pixel <jabatavadze430@gmail.com>
2026-05-14 17:24:54 +0000

    restaurant class

---
+++ ex1.py
@@ -0,0 +1,20 @@
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print(f"Restaurant name: {self.restaurant_name}")
+        print(f"Cuisine type: {self.cuisine_type}")
+
+    def open_restaurant(self):
+        print(f"{self.restaurant_name} is now open!")
+
+
+restaurant = Restaurant("Tbilisi Bistro", "Georgian")
+
+print(restaurant.restaurant_name)
+print(restaurant.cuisine_type)
+
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
